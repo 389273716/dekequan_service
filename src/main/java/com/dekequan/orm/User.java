62fc94c01b05b23dbcaaf5610ef1f64adb9ae9c8
tangtaiming <accountnum@sina.cn>
2016-08-18 00:19:51 +0800

    提交项目需要数据库查询 mybatis配置与jar包配置

--- src/main/java/com/dekequan/orm/User.java
+++ src/main/java/com/dekequan/orm/User.java
@@ -1,9 +1,8 @@
 package com.dekequan.orm;
 /**
- * 
- * <p>介绍 用户类</p>
- * @author 唐太明
- * @date 2016年8月17日 上午7:37:20
+ * <p>介绍</p>
+ * @author TTM
+ * @date 2016年8月17日 下午11:40:02
  * @version 1.0
  */
 public class User {
