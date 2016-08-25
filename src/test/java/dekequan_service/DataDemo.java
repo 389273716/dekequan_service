5ee78c168fac081fa38431b231478d5719634728
tangtaiming <accountnum@sina.cn>
2016-08-26 00:41:02 +0800

    提交数据库测试类

---
+++ src/test/java/dekequan_service/DataDemo.java
@@ -0,0 +1,26 @@
+package dekequan_service;
+
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/com/dekequan/spring/applicationContext.xml")
+public class DataDemo {
+
+	@Resource
+	private DriverManagerDataSource dataSource;
+	
+	//数据库连接测试
+	@Test
+	public void dbTest() throws SQLException {
+		System.out.println(dataSource.getConnection());
+	}
+	
+}
