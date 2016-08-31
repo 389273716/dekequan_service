1ec02ef9a1ad30e4f5550e70d6e56fef1cfacffe
WestBookQin <499288347@qq.com>
2016-09-01 00:02:04 +0800

    qzr 注册

--- src/main/java/com/dekequan/library/UserUtil.java
+++ src/main/java/com/dekequan/library/UserUtil.java
@@ -7,7 +7,9 @@ package com.dekequan.library;
  * @author qzr
  *
  */
-public class LoginUtil {
+public class UserUtil {
+	public static final String DEFAULT_NICK_NAME = "这个人很懒,什么也没有留下!";
+	public static final Integer DEFAULT_SEX = 1;//系统默认为男
 	/**
 	 * 校验验证码有效性
 	 * @param code
