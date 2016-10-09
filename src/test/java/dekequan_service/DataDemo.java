7122e0b2f256f1791e5773cccb65b29b01cb9fb3
tangtaiming <accountnum@sina.cn>
2016-10-09 19:08:48 +0800

    提交扫描位置

--- src/test/java/dekequan_service/DataDemo.java
+++ src/test/java/dekequan_service/DataDemo.java
@@ -1,8 +1,9 @@
 package dekequan_service;
 
-import com.dekequan.dao.user.UserDaoIm;
-import com.dekequan.library.utils.Print;
-import com.dekequan.service.user.UserService;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+
 import org.apache.commons.dbcp.BasicDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
-import java.sql.SQLException;
+import com.dekequan.service.user.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/com/dekequan/spring/applicationContext.xml", "classpath:/com/dekequan/spring/dispatcher-servlet.xml"})
@@ -30,9 +30,7 @@ public class DataDemo {
 //		Print.print(userService.fetchSimpleUserById(1));
 //		ApplicationContext context = new ClassPathXmlApplicationContext("com/dekequan/spring/applicationContext.xml");
 //		SqlSessionTemplate sqlSessionTemplate = (SqlSessionTemplate) context.getBean("sqlSessionTemplate");
-		UserDaoIm userDaoIm = new UserDaoIm();
 //		Print.print(userDaoIm.get(QueryHelper.where("userId", QueryHelper.EQ, 1)));
-		Print.print(userDaoIm.get(1));
 	}
 	
 }
